eb6170c9878490d877dcb0f423999f151c3a78eb
Geesa Fernando <geesaf@embla.asia>
2024-10-12 09:47:27 +0530

    2nd commit

--- src/functions/myHttpFunction.js
+++ src/functions/myHttpFunction.js
@@ -8,6 +8,6 @@ app.http('myHttpFunction', {
 
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}!` };
+        return { body: `Hellow, ${name}!` };
     }
 });
